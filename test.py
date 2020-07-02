6e1723500745e188a5faa5b063c0992cff14d3dc
GregorOvsenjak <gregor.ovsenjak@gmail.com>
2020-07-02 08:53:29 +0200

    File for script testing!

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from Visualization.Feature_importance import FeatImp
 
 
 data = pd.read_csv('data/avocado.csv')
+print(data.dtypes)
+data_1 = data.drop(['Date','type','region'],axis =1)
+fi = FeatImp(data_1)
+fi.tree_selection(target_name='AveragePrice')
+fi.corr_matrix(target_name='AveragePrice',method_version=2)
