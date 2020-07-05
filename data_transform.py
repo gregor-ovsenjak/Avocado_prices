af97a9d8a8e96be11663788cf7a495a1f22b4846
GregorOvsenjak <gregor.ovsenjak@gmail.com>
2020-07-05 10:18:26 +0200

    Done with data_transform, fully loaded and ready

--- data_transform.py
+++ data_transform.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 class Log_Transform(BaseEstimator, TransformerMixin):
-    
+    #log transformation of numerical features 
     def __init__(self,columns):
         self.columns = columns
 
@@ -39,20 +39,19 @@ class DataFrameSelector(BaseEstimator,TransformerMixin):
         return X[self.attribute_names].values
 
 
+
 class Transformations(BaseEstimator,TransformerMixin):
     '''class for numerical and categorical transformations, takes in numerical column names,
-       and categorical column names. Applies log-transformation, StandardScale tranformation
+       and categorical column names. Applies log-transformation
        to numerical attributes and OneHotEncoding on categorical attributes.
     '''
     def __init__(self,number_col,category_col):
 
         self.number_col = number_col
         self.category_col = category_col
-        self.Sc = StandardScaler()
         self.encoder = OneHotEncoder(sparse=False)
         self.number_pipeline = Pipeline([('dataFS_cat',DataFrameSelector(self.number_col)),
                                         ('Log',Log_Transform(self.number_col)),
-                                        ('Std_scaler',self.Sc),
                                         ])
         self.cat_pipeline = Pipeline([ ('dataFS_cat',DataFrameSelector(self.category_col)),
                                        ('label_binarizer', self.encoder),    
@@ -69,7 +68,8 @@ class Transformations(BaseEstimator,TransformerMixin):
             return self.full_pipeline.transform(X)
 
 
-def avocado_transformations(data):
+
+def transform_date_to_month(data):
     # Changes dtype of 'year' feature from int to object
     data['year'] = data['year'].apply(str)
     # gets only month values from 'Date' feature
@@ -78,5 +78,42 @@ def avocado_transformations(data):
     data['month'] = months
     # drops columns that are no longer of use 
     data = data.drop(['Date'],axis = 1)
-    data = data.drop(['Unnamed: 0'],axis = 1)
     return data
+
+
+
+def back_to_Data_Frame(data):
+
+    new_col_names = []
+    # appending all unique values of each feature
+    for column_name in data.columns:
+        # if dtype == int,float..., then append feature name
+        if data[column_name].dtype == np.number:
+            new_col_names.append(column_name)
+        # if dtype == object, then append each unique value of feature
+        elif data[column_name].dtype == np.object:
+            new_col_names.extend(sorted(data[column_name].unique()))
+    return new_col_names
+
+
+
+def extract_cat_num_names(data):
+    #get categorical and numerical names
+    categorical_col = data.select_dtypes([np.object]).columns
+    number_col = data.select_dtypes([np.number]).columns
+    return categorical_col,number_col
+
+
+
+def apply_transformations(data):
+    '''Apply transformations in order:
+     --> transform Date feature --> month feature
+     --> log-transformation to numerical features, OneHotEncoder to cat features
+     --> return np.array() back to DataFrame with new column names
+    '''
+    data = transform_date_to_month(data)
+    cat_names,num_names = extract_cat_num_names(data)
+    transformer = Transformations(num_names,cat_names)
+    data_1 = transformer.fit_and_transform(data)
+    return pd.DataFrame(data_1, columns= back_to_Data_Frame(data))
+
